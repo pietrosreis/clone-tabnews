2200256e2d2109dd47b1a9f1588164ea126c6359
Pietro <pietroreissilvaoliveira@gmail.com>
2026-02-27 22:31:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-function home(){
-    return <h1>Olá Bel, hihi</h1>
+function home() {
+  return <h1>Olá Bel, hihi</h1>;
+}
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("testando");
 }
 export default home;
